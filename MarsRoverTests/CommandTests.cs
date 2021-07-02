283a24b5c6ba48679372c087382649c71771fa89
The_Lump <thelump@lump.com>
2021-07-02 12:35:08 -0400

    Finished commandTests

--- MarsRoverTests/CommandTests.cs
+++ MarsRoverTests/CommandTests.cs
@@ -34,6 +34,12 @@ namespace MarsRoverTests
             Command newCommand = new Command("MOVE", 20);
             Assert.AreEqual(newCommand.NewPostion, 20);
         }
+        [TestMethod]
+        public void ConstructorSetsNewModeValue()
+        {
+            Command newCommand = new Command("CHANGE_MODE", "LOW_POWER_MODE");
+            Assert.AreEqual(newCommand.NewMode, "LOW_POWER_MODE");
+        }
 
     }
 }
